4ba2c2533edfd67fc39786d2ec73bf453a8cd252
Evyatarbe <evyevy12@gmail.com>
2024-12-26 08:19:15 +0200

    opened server

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose
+.connect('mongodb://localhost:27017/accompanying_friend', { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('MongoDB connected successfully'))
+.catch((err) => console.log('MongoDB connection error:' , err));
+
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'Server is up and running!' });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
