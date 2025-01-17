8631391df938ebaabdb69e4364d3f2369b095cf3
Natan <155454141+NatanProtector@users.noreply.github.com>
2025-01-17 22:38:01 +0200

    Adding some foundations for settings display

--- react-native-app/src/screens/SettingsDisplay.js
+++ react-native-app/src/screens/SettingsDisplay.js
@@ -1,9 +1,21 @@
 import { View, Text } from 'react-native';
+import CategoryTemplate from './SettingsCategories/CategoryTemplate';
 
-export default SettingsDisplay = ({categories}) => {
+import { useContext } from 'react';
+import MyLanguageContext from '../utils/MyLanguageContext';
+import { Button } from 'react-native-elements';
+
+export default SettingsDisplay = ({route}) => {
+
+    const { language } = useContext(MyLanguageContext);
+    const { role } = route.params || {}; // Get the Settings Display type (citizen/security)
+
     return (
         <View>
-            <Text>Settings</Text>
+            <CategoryTemplate title={"Category 1"}>
+                <Button title="example1" onPress={() => {console.log("button 1 pressed")}} />
+                <Button title="example2" onPress={() => {console.log("button 2 pressed")}} />
+            </CategoryTemplate>
         </View>
     );
 }
