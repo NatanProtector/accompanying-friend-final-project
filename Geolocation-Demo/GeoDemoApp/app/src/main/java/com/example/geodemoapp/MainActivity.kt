dfbcbfe9090c4b5096f7f879bd023cd31ade32ba
Natan <155454141+NatanProtector@users.noreply.github.com>
2025-03-09 19:08:30 +0200

    Changing compile target sdk. removing boilerplate code from main. after running for the first time, some gradle changes happened.
    changing compile target sdk
    minor changes to gradle after first run
    simplifying main

--- Geolocation-Demo/GeoDemoApp/app/src/main/java/com/example/geodemoapp/MainActivity.kt
+++ Geolocation-Demo/GeoDemoApp/app/src/main/java/com/example/geodemoapp/MainActivity.kt
@@ -8,9 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.geodemoapp.ui.theme.GeoDemoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +18,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             GeoDemoAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Text(
+                        text = "Hello World!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GeoDemoAppTheme {
-        Greeting("Android")
     }
 }
