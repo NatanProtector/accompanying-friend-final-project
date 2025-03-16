212fcfb30834bf67d48b460f2ccfb7e1a4a822b2
Evyatarbe <evyevy12@gmail.com>
2025-03-16 10:59:54 +0200

    Update HomeScreen.js

--- react-native-app/src/screens/HomeScreen.js
+++ react-native-app/src/screens/HomeScreen.js
@@ -10,25 +10,52 @@ export default function HomeScreen({ navigation }) {
     const { language } = useContext(MyLanguageContext);
 
     return (
-        <View style={style.container}>
-            <Card title={HomeText[language].title}>
-                <Button
-                    title={HomeText[language].login}
-                    onPress={() => navigation.navigate('Login')}
-                />
-                <Button
-                    title={HomeText[language].register}
-                    onPress={() => navigation.navigate('Register')}
-                />
-                <Button
-                    title={HomeText[language].map}
-                    onPress={() => navigation.navigate('Map')}
-                />
+        <View style={styles.container}>
+            <Card containerStyle={styles.card} title={HomeText[language].title}>
+                <View style={styles.buttonContainer}>
+                    <Button
+                        title={HomeText[language].login}
+                        onPress={() => navigation.navigate('Login')}
+                        buttonStyle={styles.button}
+                    />
+                    <Button
+                        title={HomeText[language].register}
+                        onPress={() => navigation.navigate('Register')}
+                        buttonStyle={styles.button}
+                    />
+                    <Button
+                        title={HomeText[language].map}
+                        onPress={() => navigation.navigate('Map')}
+                        buttonStyle={styles.button}
+                    />
+                </View>
             </Card>
         </View>
     );
 }
-
-const style = StyleSheet.create({
-    container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+    
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '95%',
+        padding: 10,
+    },
+    card: {
+        borderWidth: 2,
+        borderColor: '#000',
+        width: '100%',
+        padding: 20,
+    },
+    buttonContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        height: '100%',
+    },
+    button: {
+        marginVertical: 10,
+        borderWidth: 2,
+        borderColor: '#000',
+    },
 });
