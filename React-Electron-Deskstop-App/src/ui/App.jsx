3127995217d972c076a9dc8f474aa37f593ae172
Natan <155454141+NatanProtector@users.noreply.github.com>
2025-03-28 12:39:58 +0300

    Added basic navigation

--- React-Electron-Deskstop-App/src/ui/App.jsx
+++ React-Electron-Deskstop-App/src/ui/App.jsx
@@ -1,7 +1,38 @@
-import MyMap from './componants/MyMap.jsx';
+import React from 'react';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import MyMap from './components/MyMap';
+
+const Home = () => <h2>Home Page</h2>;
+
+const ManageUsers = () => <h2>Manage Users Page</h2>;
+
 const App = () => {
   return (
-      <MyMap />
+    <Router>
+      <div>
+        {/* Navigation links */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Manage/Users">Manage Users</Link>
+            </li>
+            <li>
+              <Link to="/map">Map</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Define routes for different components */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Manage/Users" element={<ManageUsers />} />
+          <Route path="/map" element={<MyMap />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
