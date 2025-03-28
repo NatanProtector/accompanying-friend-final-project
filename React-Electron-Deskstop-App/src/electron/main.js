71ab48538dce9ec52a21f0e2380b3b434fd94a99
Natan <155454141+NatanProtector@users.noreply.github.com>
2025-03-28 11:58:22 +0300

    Removing menu bar

--- React-Electron-Deskstop-App/src/electron/main.js
+++ React-Electron-Deskstop-App/src/electron/main.js
@@ -1,11 +1,13 @@
-import {app, BrowserWindow} from 'electron'
+import {app, BrowserWindow, Menu } from 'electron'
 import path from 'path'
 
 
 app.on("ready", () => {
     const mainWindow = new BrowserWindow({
-        
     })
+
+    // Disable the default menu
+    Menu.setApplicationMenu(null);
 
     mainWindow.loadFile(path.join(app.getAppPath(), "dist-react", "index.html"))
 })
