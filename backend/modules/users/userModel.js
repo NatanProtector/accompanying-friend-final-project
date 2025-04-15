de0b04f670ab5005c922cc60741640ed234af30f
Natan <155454141+NatanProtector@users.noreply.github.com>
2025-04-15 12:11:40 +0300

    Adding emailVerified, verificationToken, passwordResetToken, and passwordResetExpires fields and verification token to the user schema, in preperation for email verification system.

--- backend/modules/users/userModel.js
+++ backend/modules/users/userModel.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require("uuid");
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
@@ -10,31 +10,41 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   idNumber: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
+  emailVerified: { type: Boolean, default: false },
+  verificationToken: { type: String, unique: true },
+  passwordResetToken: { type: String },
+  passwordResetExpires: { type: Date },
   idPhoto: { type: String, required: false }, // Temporarily stored
   multiRole: { type: [String], required: true },
-  registrationStatus: { type: String, default: 'pending' }, // Default status
+  registrationStatus: { type: String, default: "pending" }, // Default status
   location: {
     type: { type: String, enum: ["Point"], default: "Point" }, // Required for GeoJSON
     coordinates: { type: [Number], default: [0, 0] }, // [longitude, latitude]
   },
 });
 
-
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+
+  if (this.isNew) {
+    this.verificationToken = uuidv4();
+  }
+
   next();
 });
 
-
 // Add a static method to validate conditional fields
 userSchema.statics.validateFields = function (data) {
-  if (data.multiRole.includes('security') && !data.securityCertificatePhoto) {
-    throw new Error('Security certificate photo is required for security role.');
+  if (data.multiRole.includes("security") && !data.securityCertificatePhoto) {
+    throw new Error(
+      "Security certificate photo is required for security role."
+    );
   }
-}
+};
 
-const Users = mongoose.model('Users', userSchema, 'Users');
+const Users = mongoose.model("Users", userSchema, "Users");
 module.exports = Users;
